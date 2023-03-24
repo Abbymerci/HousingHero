b50b12329915cf74fed471898946c069a24b5575
Efosa-3012 <57777213+Efosa-3012@users.noreply.github.com>
2023-03-24 03:13:35 -0400

    Date Retrieve Works but not in order

--- streak.js
+++ streak.js
@@ -2,7 +2,7 @@ Parse.initialize("kZh3SCYIQnQ3OkI1Euet7scVhDLkgwfyNai57hqj", "FbBxp4yolPxAb8ALib
 Parse.serverURL = "https://parseapi.back4app.com/";
 
 var setid = "9ZMGDCEAaF"
-
+var DateArr = []
 
 // Initialize Parse with your Back4App Application ID and JavaScript Key
 
@@ -18,17 +18,36 @@ const query = new Parse.Query(className);
 
 // Use the equalTo method to add a constraint to the query
 query.equalTo("ID", objectId);
-
+query.ascending("PaymentDate")
 // Use the find method to retrieve all the objects that match the query
 query.find().then((results) => {
-  //console.log("Retrieved ", results.length, " rows with ID ", objectId);
-  //console.log("Results: ", results);
-
   for (let i = 0; i < results.length; i++){
-    console.log(results[i])
+    var add = results[i].id
+    console.log(add)
+
+    let nquery = new Parse.Query(className);
+    nquery.equalTo("objectId", add);
+    nquery.ascending("PaymentDate")
+    nquery.first().then(function(pet){
+        if(pet){
+           console.log(typeof pet.get("PaymentDate"));
+           DateArr.push(pet.get("PaymentDate"))
+        } else {
+           console.log("Nothing found, please try again");
+        }
+    }).catch(function(error){
+        console.log("Error: " + error.code + " " + error.message);       
+    });
+
   }
 }, (error) => {
   console.error("Error while retrieving rows with ID ", objectId, ": ", error);
 });
+console.log(DateArr.sort())
+
+
+
 }
+
+
 document.onload = test();
